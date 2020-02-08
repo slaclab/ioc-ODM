b080db255b1c813db35c67dd470a803df45173d3
gwbrown <gwbrown@slac.stanford.edu>
2020-02-08 00:27:30 -0800

    New app for oxygen defficiency monitor

---
+++ iocBoot/sioc-cp00-od01/st.cmd
@@ -0,0 +1,57 @@
+#!../../bin/rhel6-x86_64/odm
+#==============================================================
+#
+#  Abs:  Startup Script for the Cryoplant ODM
+#
+#  Name: st.cmd
+#
+#  Desc:  This is the EPICS startup script for a soft IOC
+#         for the ODM PLC
+#
+#  Facility:  LCLS Personnel Protection System (PPS)
+#
+#  Auth: 20-May-2019, Anthony Andrews  (AANDREWS)
+#  Rev:  dd-mmm-yyyy, Reviewer's Name  (USERNAME)
+#--------------------------------------------------------------
+#  Mod:
+#         14-Oct-2019, K. Luchini      (LUCHINI):
+#           load common scripts
+#           load caPutLogInit
+#
+#==============================================================
+#
+
+# Set environment variables
+epicsEnvSet("IOC_NAME"  ,"SIOC:CP00:OD01")
+epicsEnvSet("LOCATION"  ,"lcls-daemon1")
+
+# Siemens supervisory PLC Node
+# Note cannot use nodename must use ip
+epicsEnvSet("ODM_NODE"      ,"172.27.14.xxx")
+
+# Load generic environment variables and database
+< ../common/st.cmd.soft
+epicsEnvSet("IOC","sioc-cp00-op01")
+
+# Initialize Modbus communications
+cd ${IOC_BOOT}
+< init_asyn.cmd
+cd ${TOP}
+
+# Load Additional databases:
+dbLoadRecords("db/odm-cp00.db")
+
+# Setup autosave/restore
+< iocBoot/common/init_restore.cmd.soft
+# Initialize caPutLog
+caPutLogInit("${EPICS_CA_PUT_LOG_ADDR}",0)
+# Start autosave routines to save our data
+< iocBoot/common/restore.cmd.soft
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=gwbrown"
+
+# End of file st.cmd
