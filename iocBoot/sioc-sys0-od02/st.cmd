6e63c213755ed907b442b42fab9e3a24283febd2
Michael Dunning <mdunning@slac.stanford.edu>
2021-04-14 17:50:54 -0700

    Sensor cell O2 monitors: update PV names

--- iocBoot/sioc-sys0-od02/st.cmd
+++ iocBoot/sioc-sys0-od02/st.cmd
@@ -33,15 +33,15 @@ iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm.iocsh", "BKH_PORT=BKH08,BKH_IP=apc
 iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm.iocsh", "BKH_PORT=BKH09,BKH_IP=apc-li07-od01")
 
 # **** Load Beckhoff databases ****
-dbLoadRecords("db/bkh_odm_li00.db", "P=ODM:LI00,M=BKH01,LOC=L2KA01-08")
-dbLoadRecords("db/bkh_odm_li01.db", "P=ODM:LI01,M=BKH02,LOC=L2KA01-08")
-dbLoadRecords("db/bkh_odm_li02.db", "P=ODM:LI02,M=BKH03,LOC=L2KA02-09")
-dbLoadRecords("db/bkh_odm_li03.db", "P=ODM:LI03,M=BKH04,LOC=L2KA03-07")
-dbLoadRecords("db/bkh_odm_supervisor.db", "P=ODM:LI03:OD01,M=BKH05,LOC=L2KA03-07")
-dbLoadRecords("db/bkh_odm_li04.db", "P=ODM:LI04,M=BKH06,LOC=L2KA04-06")
-dbLoadRecords("db/bkh_odm_li05.db", "P=ODM:LI05,M=BKH07,LOC=L2KA05-07")
-dbLoadRecords("db/bkh_odm_li06.db", "P=ODM:LI06,M=BKH08,LOC=L2KA06-08")
-dbLoadRecords("db/bkh_odm_li07.db", "P=ODM:LI07,M=BKH09,LOC=L2KA07-03")
+dbLoadRecords("db/bkh_odm_li00.db", "P=ODM:LI00,M=BKH01,BKH=BKH:LI00:OD01,LOC=L2KA01-08")
+dbLoadRecords("db/bkh_odm_li01.db", "P=ODM:LI01,M=BKH02,BKH=BKH:LI01:OD01,LOC=L2KA01-08")
+dbLoadRecords("db/bkh_odm_li02.db", "P=ODM:LI02,M=BKH03,BKH=BKH:LI02:OD01,LOC=L2KA02-09")
+dbLoadRecords("db/bkh_odm_li03.db", "P=ODM:LI03,M=BKH04,BKH=BKH:LI03:OD01,LOC=L2KA03-07")
+dbLoadRecords("db/bkh_odm_supervisor.db", "P=ODM:LI03:OD02,M=BKH05,BKH=BKH:LI03:OD02,LOC=L2KA03-07")
+dbLoadRecords("db/bkh_odm_li04.db", "P=ODM:LI04,M=BKH06,BKH=BKH:LI04:OD01,LOC=L2KA04-06")
+dbLoadRecords("db/bkh_odm_li05.db", "P=ODM:LI05,M=BKH07,BKH=BKH:LI05:OD01,LOC=L2KA05-07")
+dbLoadRecords("db/bkh_odm_li06.db", "P=ODM:LI06,M=BKH08,BKH=BKH:LI06:OD01,LOC=L2KA06-08")
+dbLoadRecords("db/bkh_odm_li07.db", "P=ODM:LI07,M=BKH09,BKH=BKH:LI07:OD01,LOC=L2KA07-03")
 
 iocInit
 
