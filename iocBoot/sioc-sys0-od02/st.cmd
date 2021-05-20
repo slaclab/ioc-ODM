7f25123df11302852da0d70104c2b4b94aa5c16f
Michael Dunning <mdunning@slac.stanford.edu>
2021-05-20 13:32:01 -0700

    Merge databases for sioc-sys0-od02 into single db; add current (mA) PVs

--- iocBoot/sioc-sys0-od02/st.cmd
+++ iocBoot/sioc-sys0-od02/st.cmd
@@ -21,27 +21,19 @@ epicsEnvSet("LOCATION"  ,"lcls-daemon1")
 # Set up autosave/restore
 < $(TOP)/iocBoot/common/init_restore.cmd.soft
 
-# **** Driver setup for Beckhoffs ****
+# **** Beckhoff driver initialization ****
 iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm.iocsh", "BKH_PORT=BKH01,BKH_IP=apc-li01-od01")
 iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm.iocsh", "BKH_PORT=BKH02,BKH_IP=apc-li01-od02")
 iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm.iocsh", "BKH_PORT=BKH03,BKH_IP=apc-li02-od01")
 iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm.iocsh", "BKH_PORT=BKH04,BKH_IP=apc-li03-od01")
-iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm_supervisor.iocsh", "BKH_PORT=BKH05,BKH_IP=apc-li03-od02")
-iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm.iocsh", "BKH_PORT=BKH06,BKH_IP=apc-li04-od01")
-iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm.iocsh", "BKH_PORT=BKH07,BKH_IP=apc-li05-od01")
-iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm.iocsh", "BKH_PORT=BKH08,BKH_IP=apc-li06-od01")
-iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm.iocsh", "BKH_PORT=BKH09,BKH_IP=apc-li07-od01")
+iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm.iocsh", "BKH_PORT=BKH05,BKH_IP=apc-li04-od01")
+iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm.iocsh", "BKH_PORT=BKH06,BKH_IP=apc-li05-od01")
+iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm.iocsh", "BKH_PORT=BKH07,BKH_IP=apc-li06-od01")
+iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm.iocsh", "BKH_PORT=BKH08,BKH_IP=apc-li07-od01")
+iocshLoad("$(TOP)/iocBoot/common/init_bkh_odm_supervisor.iocsh", "BKH_PORT=BKH09,BKH_IP=apc-li03-od02")
 
-# **** Load Beckhoff databases ****
-dbLoadRecords("db/bkh_odm_li00.db", "P=ODM:LI00,M=BKH01,BKH=BKH:LI00:OD01,LOC=L2KA01-08")
-dbLoadRecords("db/bkh_odm_li01.db", "P=ODM:LI01,M=BKH02,BKH=BKH:LI01:OD01,LOC=L2KA01-08")
-dbLoadRecords("db/bkh_odm_li02.db", "P=ODM:LI02,M=BKH03,BKH=BKH:LI02:OD01,LOC=L2KA02-09")
-dbLoadRecords("db/bkh_odm_li03.db", "P=ODM:LI03,M=BKH04,BKH=BKH:LI03:OD01,LOC=L2KA03-07")
-dbLoadRecords("db/bkh_odm_supervisor.db", "P=ODM:LI03:OD02,M=BKH05,BKH=BKH:LI03:OD02,LOC=L2KA03-07")
-dbLoadRecords("db/bkh_odm_li04.db", "P=ODM:LI04,M=BKH06,BKH=BKH:LI04:OD01,LOC=L2KA04-06")
-dbLoadRecords("db/bkh_odm_li05.db", "P=ODM:LI05,M=BKH07,BKH=BKH:LI05:OD01,LOC=L2KA05-07")
-dbLoadRecords("db/bkh_odm_li06.db", "P=ODM:LI06,M=BKH08,BKH=BKH:LI06:OD01,LOC=L2KA06-08")
-dbLoadRecords("db/bkh_odm_li07.db", "P=ODM:LI07,M=BKH09,BKH=BKH:LI07:OD01,LOC=L2KA07-03")
+## Load databases
+dbLoadRecords("db/odm-sys0-od02.db")
 
 iocInit
 
